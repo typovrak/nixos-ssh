d6e3dd9221f7808180924a9cd3dfa5c663bbec4b
typovrak <typovrak@gmail.com>
2025-04-28 21:15:58 +0200

    add var for username

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 
 let
-	username = "typovrak";
+	username = config.username;
 	group = config.users.users.${username}.group or "users";
 	home = config.users.users.${username}.home;
 in {
 	system.activationScripts.ssh = ''
+		# no rm for keeping credentials, adding -p
 		mkdir -p ${home}/.ssh
 		chown ${username}:${group} ${home}/.ssh
 		chmod 700 ${home}/.ssh
