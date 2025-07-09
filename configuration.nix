0fa4d7bd59e31f99c3f500d548cbc7e3e72a71fd
typovrak <typovrak@gmail.com>
2025-07-09 22:06:47 +0200

    add signin template

--- configuration.nix
+++ configuration.nix
@@ -11,10 +11,13 @@ in {
 		chown ${username}:${group} ${home}/.ssh
 		chmod 700 ${home}/.ssh
 
-
 		cp ${./config} ${home}/.ssh/config
 		chown ${username}:${group} ${home}/.ssh/config
 		chmod 600 ${home}/.ssh/config
+
+		cp ${./allowed_signers} ${home}/.ssh/allowed_signers
+		chown ${username}:${group} ${home}/.ssh/allowed_signers
+		chmod 600 ${home}/.ssh/allowed_signers
 	'';
 
 	environment.systemPackages = with pkgs; [
